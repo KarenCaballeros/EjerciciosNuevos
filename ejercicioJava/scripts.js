0b932d9951660d55bb0b1f0adcb6a08598712e08
KarenCaballeros <anakarencaballeros@gmail.com>
2017-05-23 11:06:10 -0600

    removi clase de cuadrado.

--- ejercicioJava/scripts.js
+++ ejercicioJava/scripts.js
@@ -1,6 +1,11 @@
 var square = document.getElementById("square");
 var selColor = document.getElementById("color");
 var botonCrear = document.getElementById("crear");
+var inCantidad = document.getElementById("cantidad");
 botonCrear.onclick = function(){
+	square.className = 'square';
 	square.classList.add(selColor.value);
+	var cantidad = parseInt(inCantidad.value);
+
+	
 }
