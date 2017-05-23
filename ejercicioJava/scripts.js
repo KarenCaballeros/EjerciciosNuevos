1b2e0584674c6e1349772262929e8fec68246e5a
KarenCaballeros <anakarencaballeros@gmail.com>
2017-05-23 11:45:56 -0600

    cambio.

--- ejercicioJava/scripts.js
+++ ejercicioJava/scripts.js
@@ -1,11 +1,16 @@
-var square = document.getElementById("square");
+var Square = document.getElementById("square");
 var selColor = document.getElementById("color");
 var botonCrear = document.getElementById("crear");
 var inCantidad = document.getElementById("cantidad");
 botonCrear.onclick = function(){
-	square.className = 'square';
+	square.className = "square";
 	square.classList.add(selColor.value);
 	var cantidad = parseInt(inCantidad.value);
-
-	
+	var cont = ""
+	for (i = 0; i < cantidad ; i++) {
+    	cont += square
+    }
+   caja.innerHTML = cont
 }
+
+//<div class='square'></div>"
